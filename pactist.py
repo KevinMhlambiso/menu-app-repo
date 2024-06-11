9da94f75505511c5ce14570f5b47f3745d4a1069
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-06-11 16:03:24 +0200

    Update pactist.py

--- pactist.py
+++ pactist.py
@@ -1,22 +1,89 @@
 from tkinter import *
 from tkinter import filedialog
+import subprocess
+import platform
 
-# def click():
+def openFile():
+    filepath = filedialog.askopenfilename(initialdir="",title="open yo file",filetypes=(("text file","*.txt"),("all file","*-*")))
+    file = open(filepath,'r')
+    print(file.read())
+    file.close()
 
 def saveFile():
     file = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("text file",".txt"),("HTML file",".HTML"),("all files",".*")])
-    file.write(filetext)
-    filetext = input("enter text: ")
-    filetext = str(text.get(1.0,END))
+    # file.write(fileText)
+    # fileText = input("enter text: ")
+    # fileText = str(text.get(1.0,END))
     file.close()
- 
+
+
+def cutFile(source, destination):
+    system = platform.system()
+    if system == 'Windows':
+        subprocess.run(['move', source, destination], shell=True, check=True)
+    else:
+        subprocess.run(['mv', source, destination], check=True)
+
+
+source = 'path/to/source/file.txt'
+destination = 'path/to/destination/file.txt'
+
+cutFile(source, destination)
+print(f"File moved from {source} to {destination}")
+
+
+def copyFile(source, destination):
+    system = platform.system()
+    if system == 'Windows':
+        subprocess.run(['copy', source, destination], shell=True, check=True)
+    else:
+        subprocess.run(['cp', source, destination], check=True)
+
+
+source = 'path/to/source/file.txt'
+destination = 'path/to/destination/file.txt'
+copyFile(source, destination)
+
+
+
+def pasteFile(source, destination):
+    system = platform.system()
+    if system == 'Windows':
+        subprocess.run(['copy', source, destination], shell=True, check=True)
+    else:
+        subprocess.run(['cp', source, destination], check=True)
+
+
+source = 'path/to/source/file.txt'
+destination = 'path/to/destination/file.txt'
+
+pasteFile(source, destination)
+print(f"File pasted from {source} to {destination}")
+
+
 windows = Tk()
 
+fileImage = PhotoImage(file="file.png")
+saveImage = PhotoImage(file="save.png")
+exitImage = PhotoImage(file="exit.png")
+cutImage = PhotoImage(file="cut.png")
+copyImage = PhotoImage(file="copy.png")
+pasteImage = PhotoImage(file="paste.png")
 
-text = Text(windows)
-text.pack()
+menubar = Menu(windows)
+windows.config(menu=menubar)
 
-button = Button(windows,command=saveFile,text='open')
-button.pack()
+fileMenu = Menu(menubar,tearoff=0)
+menubar.add_cascade(label="file",menu=fileMenu)
+fileMenu.add_command(label="open",command=openFile,image=fileImage,compound='left')
+fileMenu.add_command(label="save",command=saveFile,image=saveImage,compound='left')
+fileMenu.add_separator()
+fileMenu.add_command(label="exit",command=quit,image=exitImage,compound='left')
+
+editMenu = Menu(menubar,tearoff=0)
+menubar.add_cascade(label="Edit",menu=editMenu)
+editMenu.add_command(label="cut",command=cutFile,image=cutImage,compound='right')
+editMenu.add_command(label="copy",command=copyFile,image=copyImage,compound='right')
+editMenu.add_command(label="paste",command=pasteFile,image=pasteImage,compound='right')
 
 windows.mainloop()
